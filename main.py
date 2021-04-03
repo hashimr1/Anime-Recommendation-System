9086fa0c90e01af6f3ea93b66785144f3e8b2f35
Tu Pham <altersaber1786@gmail.com>
2021-04-03 00:21:35 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+"""CSC111 Final Project: My Anime Recommendations
+==========================================================
+main.py
+==========================================================
+@authors: Tu Pham, Tahseen Rana, Raazia Hashim, Meet Patel.
+"""
+
+
+if __name__ == '__man__':
+    pass
